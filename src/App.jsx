10d51dd7fd902bab6dcd404e098422e9f0406c92
Keila Prado <keilaprado.pv@gmail.com>
2025-07-18 12:27:12 -0300

    feat: add login and register files, stylyng elements and adding toastify notifications

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import './App.css'
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
+import "react-toastify/dist/ReactToastify.css"
+import { ToastContainer } from "react-toastify"
 
 import Home from './pages/Home/Home'
 import About from './pages/About/About'
+import Login from './pages/Login/Login'
+import Register from './pages/Register/Register'
 
 import Navbar from './components/Navbar/Navbar'
 import Footer from './components/Footer/Footer'
@@ -17,9 +21,12 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
           </Routes>
         </div>
         <Footer />
+        <ToastContainer />
       </BrowserRouter>
     </div>
   )
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,10 +10,28 @@ const Navbar = () => {
       </NavLink>
       <ul className={styles.links_list}>
         <li>
-          <NavLink to='/'>Home</NavLink>
+          <NavLink to='/'
+            className={({ isActive }) => (isActive ? styles.active : "")}>
+              Home
+          </NavLink>
         </li>
         <li>
-          <NavLink to='/about'>Sobre</NavLink>
+          <NavLink to='/login'
+            className={({ isActive }) => (isActive ? styles.active : "")}>
+              Login
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to='/register'
+            className={({ isActive }) => (isActive ? styles.active : "")}>
+              Cadastrar
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to='/about'
+            className={({ isActive }) => (isActive ? styles.active : "")}>
+              Sobre
+          </NavLink>
         </li>
       </ul>
     </nav>
